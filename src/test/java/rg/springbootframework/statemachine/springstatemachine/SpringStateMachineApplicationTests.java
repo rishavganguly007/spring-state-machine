ecfc779834b5b1f10d560a873ad486fb2be49fb3
rishavganguly007 <rishavganguly007@gmail.com>
2024-03-30 13:55:11 +0530

    Initial commit

---
+++ src/test/java/rg/springbootframework/statemachine/springstatemachine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package rg.springbootframework.statemachine.springstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
